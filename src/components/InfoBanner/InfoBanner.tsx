0d075c1403e28fce3e5cc10a50ff4ae9c53c0417
Daniel <daniel@crosstechit.com>
2021-12-25 08:17:49 +0200

    update info banner

--- src/components/InfoBanner/InfoBanner.tsx
+++ src/components/InfoBanner/InfoBanner.tsx
@@ -1,4 +1,4 @@
-import { getChainById, Goerli, Hardhat, Kovan, Localhost, Mainnet, Rinkeby, Ropsten, useEthers } from '@usedapp/core'
+import { getChainById, Hardhat, Localhost, Mainnet, Ropsten, useEthers } from '@usedapp/core'
 import React from 'react'
 import { UnsupportedNetworkModal } from '../UnsupportedNetworkModal';
 import { StyledLink, Wrapper } from './InfoBanner.styles'
@@ -10,10 +10,13 @@ export const InfoBanner: React.FC = () => {
 
   const chain = getChainById(chainId);
   const ethChains = [Mainnet.chainId, Ropsten.chainId, Localhost.chainId, Hardhat.chainId];
+  if (chainId === Ropsten.chainId && process.env.NEXT_PUBLIC_ROPSTEN_HOSTNAME === window.location.hostname) {
+    return null;
+  }
 
   return (
     <Wrapper>
-      <p>Your wallet is connected to the {chain?.chainName} test network. To use this website on Ropsten, please switch to <StyledLink target='_blank' rel='noreferrer' href={`https://ropsten-nft.thecreatorsdesigns.com`}>ropsten-nft.thecreatorsdesigns.com</StyledLink></p>
+      <p>Your wallet is connected to the {chain?.chainName} {chain?.isTestChain ? 'test' : ''} network. To use this website on Ropsten, please switch to <StyledLink target='_blank' rel='noreferrer' href={`https://${process.env.NEXT_PUBLIC_ROPSTEN_HOSTNAME}`}>{process.env.NEXT_PUBLIC_ROPSTEN_HOSTNAME}</StyledLink></p>
       <UnsupportedNetworkModal isOpen={!ethChains.includes(chainId)} />
     </Wrapper >
   )
